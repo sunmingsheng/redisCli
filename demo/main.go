98007688e821b951d41b97b21a3f87811ce531b6
mingsheng <798181610@qq.com>
2020-05-21 19:57:28 +0800

    添加代码

--- demo/main.go
+++ demo/main.go
@@ -12,18 +12,21 @@ func main() {
 	options := redisCli.Option{
 		Addr:"127.0.0.1:6379",
 		Cluster:[]string{
-			"127.0.0.1:7100",
-			"127.0.0.1:7101",
-			"127.0.0.1:7102",
-			"127.0.0.1:7103",
-			"127.0.0.1:7104",
-			"127.0.0.1:7105",
+			"127.0.0.1:7200",
+			"127.0.0.1:7201",
+			"127.0.0.1:7202",
+			"127.0.0.1:7203",
+			"127.0.0.1:7204",
+			"127.0.0.1:7205",
 		},
 		MaxIdleTime: time.Second * 10,
 		MaxOpenConn: 30,
 		MaxIdleConn: 2,
 	}
-	client, _ := redisCli.NewClusterClient(options)
+	client, err := redisCli.NewClusterClient(options)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	go func() {
 		for {
@@ -32,16 +35,9 @@ func main() {
 		}
 	}()
 
-	num := 0
 	http.HandleFunc("/redis", func(writer http.ResponseWriter, request *http.Request) {
 
-		if _, err := client.Get("age"); err != nil && err.Error() == "无可用连接" {
-			num +=1
-		}
-		if _, err := client.Get("222"); err != nil && err.Error() == "无可用连接" {
-			num +=1
-		}
-		fmt.Println(num)
+		fmt.Println(client.Get("age"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
